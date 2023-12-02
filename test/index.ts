1b246739da927b8099a130184893a422a1f0cd48
Tahlil <bsse0803@iit.du.ac.bd>
2023-12-02 23:22:49 +0600

    Check all tests

--- test/index.ts
+++ test/index.ts
@@ -77,7 +77,7 @@ describe('password manager', () => {
     expect(pass === encryptedPassword);
   });
 
-  it.only("Check store password functionality", async function () {
+  it("Check store password functionality", async function () {
     const { passContract, otherAccounts, platform, userAddress, user } = await loadFixture(deployOnceFixture);
     
     const { messageHashBytes, v, r, s } = await signMessage(user);
