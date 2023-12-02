a9197988a3fe0eafb00c8072db95d1f3057e7b9a
Tahlil <bsse0803@iit.du.ac.bd>
2023-12-02 20:27:33 +0600

    Whitelist address and platforms

--- contracts/Pass.sol
+++ contracts/Pass.sol
@@ -4,5 +4,19 @@ pragma solidity ^0.8.15;
 import "hardhat/console.sol";
 
 contract Pass {
-    
+    // platform name + user hash
+    mapping(bytes32 => string) paltformPasswords;
+    mapping (address => bool) whitelisted;
+    mapping (string => bool) whitelistedPlatforms;
+
+    function registerFunction(address addressToCheck) external{
+        require(!whitelisted[addressToCheck], "Already reged");
+        whitelisted[addressToCheck] = true;
+    }
+
+    function registerPlatform(string calldata platform) external{
+        require(!whitelistedPlatforms[platform], "Already reged");
+        whitelistedPlatforms[platform] = true;
+    }
+
 }
