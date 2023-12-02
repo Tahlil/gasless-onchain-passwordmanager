3174097942a52cfda0f59cce3a14a9298f0302a0
Tahlil <bsse0803@iit.du.ac.bd>
2023-12-02 18:46:35 +0600

    Added falcon cryptography

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const aes256 = require("aes256");
 const fs = require("fs");
 const falcon = require("falcon-crypto");
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const keyFilePath = "key.txt";
 const textToEncrypt = "Hello, this is a secret message!";
 
-
+async function falconEncryption(msg) {
+  const keyPair /*: {privateKey: Uint8Array; publicKey: Uint8Array} */ =
+    await falcon.keyPair();
+  const message /*: Uint8Array */ = new Uint8Array([
+    104, 101, 108, 108, 111, 0,
+  ]); // "hello"
+  const signed = await falcon.sign(message, keyPair.privateKey);
+  const verified = await falcon.open(signed, keyPair.publicKey); 
+  
+  console.log({message, signed, verified});
+}
 
 async function encryptWithAES256(textToEncrypt) {
   // Check if the key file exists
@@ -31,17 +41,16 @@ async function encryptWithAES256(textToEncrypt) {
 }
 
 function decryptWithAES256(textToDecrypt) {
-    if (fs.existsSync(keyFilePath)) {
-        // If the key file exists, read the key from the file
-        const key = fs.readFileSync(keyFilePath, "utf-8");
-        console.log("Key read from key.txt");
-    
-        // Use the existing key to encrypt text
-        return decryptAES256Text(key, textToDecrypt);
-      }
-      else{
-        throw Error("Key not found")
-      }
+  if (fs.existsSync(keyFilePath)) {
+    // If the key file exists, read the key from the file
+    const key = fs.readFileSync(keyFilePath, "utf-8");
+    console.log("Key read from key.txt");
+
+    // Use the existing key to encrypt text
+    return decryptAES256Text(key, textToDecrypt);
+  } else {
+    throw Error("Key not found");
+  }
 }
 
 // Function to encrypt text using the AES key
@@ -51,9 +60,9 @@ function encryptAES256Text(key, text) {
 }
 
 function decryptAES256Text(key, text) {
-    const decrypted = aes256.decrypt(key, text);
-    return decrypted;
-  }
+  const decrypted = aes256.decrypt(key, text);
+  return decrypted;
+}
 
 function generatePassword() {
   return generator.generate({
@@ -61,7 +70,6 @@ function generatePassword() {
     numbers: true,
   });
 }
-
 
 // Function to generate a strong AES key using bcrypt and async/await
 async function generateAESKey() {
@@ -72,18 +80,17 @@ async function generateAESKey() {
     const hash = await bcrypt.hash(randomString, saltRounds);
     return hash;
   } catch (err) {
-    throw new Error('Error generating AES key: ' + err);
+    throw new Error("Error generating AES key: " + err);
   }
 }
 
-
-
 async function main() {
-    // AES 256 encryption and decryption
-    const encryptedAES = await encryptWithAES256(textToEncrypt);
-    const decryptedAES = decryptWithAES256(encryptedAES);
-    console.log({encryptedAES, decryptedAES});
+  // AES 256 encryption and decryption
+//   const encryptedAES = await encryptWithAES256(textToEncrypt);
+//   const decryptedAES = decryptWithAES256(encryptedAES);
+//   console.log({ encryptedAES, decryptedAES });
+
+   falconEncryption("")
 }
 
-
-main()
+main();
