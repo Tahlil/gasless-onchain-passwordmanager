9058c0eb288f9a3bc79a32fbe59aa4f6b52770db
Tahlil <bsse0803@iit.du.ac.bd>
2023-12-16 23:11:04 +0600

    Format code and get 5 times result on hedera

--- scripts/hederaPass.js
+++ scripts/hederaPass.js
@@ -2,7 +2,12 @@ const path = require("path");
 const fs = require("fs");
 const { artifacts } = require("hardhat");
 const ethers = require("ethers");
-const hederaPassDataPath = path.join(__dirname, "../", "results", "hederaPassUSD.json");
+const hederaPassDataPath = path.join(
+  __dirname,
+  "../",
+  "results",
+  "hederaPassUSD.json"
+);
 
 require("dotenv").config({ path: path.resolve(__dirname, "..", ".env") });
 const { hethers } = require("@hashgraph/hethers");
@@ -30,99 +35,118 @@ const client = Client.forTestnet();
 client.setOperator(operatorId, operatorPrivateKey);
 
 async function main() {
-    let startTime, endTime;
-    const contractArtifact = await artifacts.readArtifact("Pass");
-
-    const bytecode = contractArtifact.bytecode;
-
+  let startTime, endTime;
+  const contractArtifact = await artifacts.readArtifact("Pass");
+  const final = [];
+  const bytecode = contractArtifact.bytecode;
+  for (let index = 0; index < 5; index++) {
     const contractInstantiateTx = new ContractCreateFlow()
       .setBytecode(bytecode)
       .setGas(500000);
-    const contractInstantiateSubmit = await contractInstantiateTx.execute(client);
+    const contractInstantiateSubmit = await contractInstantiateTx.execute(
+      client
+    );
     const contractInstantiateRx = await contractInstantiateSubmit.getReceipt(
       client
     );
     const passContractId = contractInstantiateRx.contractId;
-	console.log(`- The Greeter smart contract ID is: ${passContractId} \n`);
+    console.log(`- The Greeter smart contract ID is: ${passContractId} \n`);
 
-    await registerFunction(passContractId, "0xa3a9d3a54630a1b8886f01038b2f9f5076025686");
+    await registerFunction(
+      passContractId,
+      "0xa3a9d3a54630a1b8886f01038b2f9f5076025686"
+    );
 
-    const obj = {}
-    
-    let timestampRegisterFunction = Date.now()
+    const obj = {};
+
+    let timestampRegisterFunction = Date.now();
     console.time("Set_Pass_Value_timer");
     startTime = performance.now();
-    let txFee = await registerFunction(passContractId, "0xcb482912Fd8461B8BF8408BA1509192930766C8B");
+    let txFee = await registerFunction(
+      passContractId,
+      "0xcb482912Fd8461B8BF8408BA1509192930766C8B"
+    );
     endTime = performance.now();
     console.timeEnd("Set_Pass_Value_timer");
-    obj["registerFunction"] = {[timestampRegisterFunction]: `${txFee}/${endTime-startTime}ms`};
+    obj["registerFunction"] = {
+      [timestampRegisterFunction]: `${txFee}/${endTime - startTime}ms`,
+    };
     console.log("Register function done");
 
-    timestampRegisterFunction = Date.now()
+    timestampRegisterFunction = Date.now();
     console.time("Set_Pass_Value_timer");
     startTime = performance.now();
     txFee = await registerPlatform(passContractId);
     endTime = performance.now();
     console.timeEnd("Set_Pass_Value_timer");
-    obj["registerPlatform"] = {[timestampRegisterFunction]: `${txFee}/${endTime-startTime}ms`};
+    obj["registerPlatform"] = {
+      [timestampRegisterFunction]: `${txFee}/${endTime - startTime}ms`,
+    };
     console.log("Register platform done");
 
-
-    timestampRegisterFunction = Date.now()
+    timestampRegisterFunction = Date.now();
     console.time("Set_Pass_Value_timer");
     startTime = performance.now();
     txFee = await storePassword(passContractId);
     endTime = performance.now();
     console.timeEnd("Set_Pass_Value_timer");
-    obj["storePassword"] = {[timestampRegisterFunction]: `${txFee}/${endTime-startTime}ms`};
+    obj["storePassword"] = {
+      [timestampRegisterFunction]: `${txFee}/${endTime - startTime}ms`,
+    };
     console.log("Store password done");
 
-    timestampRegisterFunction = Date.now()
+    timestampRegisterFunction = Date.now();
     console.time("Set_Pass_Value_timer");
     startTime = performance.now();
     txFee = await getPassword(passContractId);
     endTime = performance.now();
     console.timeEnd("Set_Pass_Value_timer");
-    obj["getPassword"] = {[timestampRegisterFunction]: `${txFee}/${endTime-startTime}ms`};
+    obj["getPassword"] = {
+      [timestampRegisterFunction]: `${txFee}/${endTime - startTime}ms`,
+    };
     console.log("Get password done");
 
-    timestampRegisterFunction = Date.now()
+    timestampRegisterFunction = Date.now();
     console.time("Set_Pass_Value_timer");
     startTime = performance.now();
     txFee = await freezeAccount(passContractId);
     endTime = performance.now();
     console.timeEnd("Set_Pass_Value_timer");
-    obj["freezeAccount"] = {[timestampRegisterFunction]: `${txFee}/${endTime-startTime}ms`};
+    obj["freezeAccount"] = {
+      [timestampRegisterFunction]: `${txFee}/${endTime - startTime}ms`,
+    };
     console.log("Freeze account done");
+    final.push(obj)
+  }
+  fs.writeFileSync(hederaPassDataPath, JSON.stringify(final), null, 2);
 
-    fs.writeFileSync(hederaPassDataPath, JSON.stringify(obj), null, 2);
-    process.exit();
-//   const filePath = path.join(
-//     __dirname,
-//     "../",
-//     "deployInfo",
-//     "deployPassHedera.json"
-//   );
-//   let passKey;
-//   try {
-//     // Read the file
-//     const data = await fs.readFileSync(filePath, "utf8");
+  process.exit();
+  //   const filePath = path.join(
+  //     __dirname,
+  //     "../",
+  //     "deployInfo",
+  //     "deployPassHedera.json"
+  //   );
+  //   let passKey;
+  //   try {
+  //     // Read the file
+  //     const data = await fs.readFileSync(filePath, "utf8");
 
-//     // Parse JSON data
-//     const jsonData = JSON.parse(data);
+  //     // Parse JSON data
+  //     const jsonData = JSON.parse(data);
 
-//     // Access the "pass" key
-//     passKey = jsonData.pass;
+  //     // Access the "pass" key
+  //     passKey = jsonData.pass;
 
-//     console.log('Value of "pass" key:', passKey);
-//   } catch (error) {
-//     console.error("Error reading/parsing the file:", error);
-//   } finally {
-//     const passContractId = passKey;
+  //     console.log('Value of "pass" key:', passKey);
+  //   } catch (error) {
+  //     console.error("Error reading/parsing the file:", error);
+  //   } finally {
+  //     const passContractId = passKey;
 
-//     // Register platform
-  
-//   }
+  //     // Register platform
+
+  //   }
 
   //   const contractAddress = contractId.toSolidityAddress();
   //   console.log(`- The Password manager smart contract ID is: ${passContractId} \n`);
@@ -141,9 +165,7 @@ async function registerFunction(passContractId, userAddress) {
     .setGas(200000)
     .setFunction(
       "registerFunction",
-      new ContractFunctionParameters().addAddress(
-        userAddress
-      )
+      new ContractFunctionParameters().addAddress(userAddress)
     );
   let contractExecuteSubmit = await contractExecuteTx.execute(client);
   let contractExecuteRx = await contractExecuteSubmit.getReceipt(client);
@@ -227,81 +249,81 @@ async function storePassword(passContractId) {
 }
 
 async function freezeAccount(passContractId) {
-    // Your string data
-    let myString = "123321asddsadkjfdkfjdkfj";
-  
-    // Truncate or pad the string to 32 bytes
-    if (myString.length > 32) {
-      myString = myString.slice(0, 32); // Truncate if longer than 32 bytes
-    } else {
-      myString = ethers.utils.formatBytes32String(myString).padEnd(66, "0"); // Pad with zeros to reach 32 bytes
-    }
-  
-    // Convert string to bytes32
-    const bytes32Param = ethers.utils.arrayify(myString);
-  
-    const wallet = new ethers.Wallet(
-      process.env.PRIVATE_KEY,
-      hethers.providers.getDefaultProvider("testnet")
-    );
-    console.log(wallet.address);
-    let signature = await wallet.signMessage(bytes32Param);
-    const r = signature.slice(0, 66);
-    const s = "0x" + signature.slice(66, 130);
-    const v = parseInt(signature.slice(130, 132), 16);
-    let contractExecuteTx = new ContractExecuteTransaction()
-      .setContractId(passContractId)
-      .setGas(200000)
-      .setFunction(
-        "freezeAccount",
-        new ContractFunctionParameters()
-          .addAddress(wallet.address + "")
-          .addAddress(solidityAddress)
-          .addBytes32(bytes32Param)
-          .addUint8(v)
-          .addBytes32(ethers.utils.arrayify(r))
-          .addBytes32(ethers.utils.arrayify(s))
-      );
-    let contractExecuteSubmit = await contractExecuteTx.execute(client);
-    let contractExecuteRx = await contractExecuteSubmit.getReceipt(client);
-  
-    let contractExecuteRec = await contractExecuteSubmit.getRecord(client);
-  
-    return (
-      (Number(contractExecuteRec.transactionFee._valueInTinybar) *
-        contractExecuteRx.exchangeRate.exchangeRateInCents) /
-      10 ** 10
-    );
+  // Your string data
+  let myString = "123321asddsadkjfdkfjdkfj";
+
+  // Truncate or pad the string to 32 bytes
+  if (myString.length > 32) {
+    myString = myString.slice(0, 32); // Truncate if longer than 32 bytes
+  } else {
+    myString = ethers.utils.formatBytes32String(myString).padEnd(66, "0"); // Pad with zeros to reach 32 bytes
   }
 
-  async function getPassword(greeterContractId) {
-	let contractExecuteTx = await new ContractExecuteTransaction()
-		.setContractId(greeterContractId)
-		.setGas(200000)
-		.setFunction("getPassword",
-        new ContractFunctionParameters()
-          .addString("Test"))
-		.execute(client);
-        let contractExecuteRx = await contractExecuteTx.getReceipt(client);
-        let contractExecuteRec = await contractExecuteTx.getRecord(client);
- 
-        return (
-          (Number(contractExecuteRec.transactionFee._valueInTinybar) *
-            contractExecuteRx.exchangeRate.exchangeRateInCents) /
-          10 ** 10
-        );   
-        // let contractExecuteSubmit = await contractExecuteTx.execute(client);
-        // let contractExecuteRx = await contractExecuteTx.getReceipt(client);
-      
-        // let contractExecuteRec = await contractExecuteTx.getRecord(client);
-      
-        // return (
-        //   (Number(contractExecuteRec.transactionFee._valueInTinybar) *
-        //     contractExecuteRx.exchangeRate.exchangeRateInCents) /
-        //   10 ** 10
-        // );    
+  // Convert string to bytes32
+  const bytes32Param = ethers.utils.arrayify(myString);
 
- }  
+  const wallet = new ethers.Wallet(
+    process.env.PRIVATE_KEY,
+    hethers.providers.getDefaultProvider("testnet")
+  );
+  console.log(wallet.address);
+  let signature = await wallet.signMessage(bytes32Param);
+  const r = signature.slice(0, 66);
+  const s = "0x" + signature.slice(66, 130);
+  const v = parseInt(signature.slice(130, 132), 16);
+  let contractExecuteTx = new ContractExecuteTransaction()
+    .setContractId(passContractId)
+    .setGas(200000)
+    .setFunction(
+      "freezeAccount",
+      new ContractFunctionParameters()
+        .addAddress(wallet.address + "")
+        .addAddress(solidityAddress)
+        .addBytes32(bytes32Param)
+        .addUint8(v)
+        .addBytes32(ethers.utils.arrayify(r))
+        .addBytes32(ethers.utils.arrayify(s))
+    );
+  let contractExecuteSubmit = await contractExecuteTx.execute(client);
+  let contractExecuteRx = await contractExecuteSubmit.getReceipt(client);
+
+  let contractExecuteRec = await contractExecuteSubmit.getRecord(client);
+
+  return (
+    (Number(contractExecuteRec.transactionFee._valueInTinybar) *
+      contractExecuteRx.exchangeRate.exchangeRateInCents) /
+    10 ** 10
+  );
+}
+
+async function getPassword(greeterContractId) {
+  let contractExecuteTx = await new ContractExecuteTransaction()
+    .setContractId(greeterContractId)
+    .setGas(200000)
+    .setFunction(
+      "getPassword",
+      new ContractFunctionParameters().addString("Test")
+    )
+    .execute(client);
+  let contractExecuteRx = await contractExecuteTx.getReceipt(client);
+  let contractExecuteRec = await contractExecuteTx.getRecord(client);
+
+  return (
+    (Number(contractExecuteRec.transactionFee._valueInTinybar) *
+      contractExecuteRx.exchangeRate.exchangeRateInCents) /
+    10 ** 10
+  );
+  // let contractExecuteSubmit = await contractExecuteTx.execute(client);
+  // let contractExecuteRx = await contractExecuteTx.getReceipt(client);
+
+  // let contractExecuteRec = await contractExecuteTx.getRecord(client);
+
+  // return (
+  //   (Number(contractExecuteRec.transactionFee._valueInTinybar) *
+  //     contractExecuteRx.exchangeRate.exchangeRateInCents) /
+  //   10 ** 10
+  // );
+}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
