850d0c9abd373fb90d85c3413ea4f14e095bd5ec
Tahlil <bsse0803@iit.du.ac.bd>
2023-12-18 10:10:42 +0600

    Get gas info

--- scripts/etherToUSD.ts
+++ scripts/etherToUSD.ts
@@ -7,5 +7,10 @@ const jsonData = JSON.parse(fs.readFileSync(contractDetailsDataPath, 'utf8'));
 
 
 for (const data of jsonData) {
-    console.log(data.registerFunction); 
+    for (const val in data) {
+        for (const det in data[val]) {
+           const gas = data[val][det].split("/")[0]
+           console.log(gas);           
+        }
+    } 
 }
